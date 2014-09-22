4389d878a34ae099401f70d3278682a6a270372c
Martin Vidner <mvidner@suse.cz>
2014-09-22 18:00:02 +0200

    it "does not translate Ops.add if any argument is ugly"

---
+++ README.md
@@ -0,0 +1,8 @@
+
+Nice and Ugly
+-------------
+
+A **nice** value is one that cannot be `nil` and is therefore suitable as an
+argument to a native operator.
+
+An **ugly** value is one that may be `nil`.
--- spec/nice_literal_spec.rb
+++ spec/nice_literal_spec.rb
@@ -6,4 +6,12 @@ describe ZombieKiller do
     c2 = '"Hello" + "World"'
     expect(ZombieKiller.new.kill(c1)).to eq c2
   end
+
+  it "does not translate Ops.add if any argument is ugly" do
+    c1 = 'Ops.add("Hello", world)'
+    expect(ZombieKiller.new.kill(c1)).to eq c1
+
+    c2 = 'Ops.add(hello, "World")'
+    expect(ZombieKiller.new.kill(c2)).to eq c2
+  end
 end
