321682403011813da2e3a7115c3afdbb2ff2ee09
Martin Vidner <mvidner@suse.cz>
2014-09-22 17:45:48 +0200

    it "translates Ops.add of two string literals" PASSES

--- lib/zombie_killer.rb
+++ lib/zombie_killer.rb
@@ -1,5 +1,51 @@
+require "parser"
+require "parser/current"
+require "unparser"
+
+class ZombieKillerRewriter < Parser::Rewriter
+  def on_send(node)
+    if is_call(node, :Ops, :add)
+      new_op = :+
+
+      ops, add, a, b = *node
+      if nice_literal(a) && nice_literal(b)
+        replace_node node, Parser::AST::Node.new(:send, [a, new_op, b])
+      end
+    end
+  end
+
+  private
+
+  def is_call(node, namespace, message)
+    n_receiver, n_message = *node
+    n_receiver.type == :const &&
+      n_receiver.children[0] == nil &&
+      n_receiver.children[1] == namespace &&
+      n_message == message
+  end
+
+  def nice_literal(node)
+    node.type == :str
+  end
+
+  def replace_node(old_node, new_node)
+    replace(old_node.loc.expression, Unparser.unparse(new_node))
+  end
+end
+
 class ZombieKiller
   def kill(s)
-    "TODO!"
+    rewriter = ZombieKillerRewriter.new
+    rewrite(s, rewriter)
+  end
+
+  private
+
+  def rewrite(code, rewriter)
+    buffer = Parser::Source::Buffer.new("(inline code)")
+    buffer.source = code
+    parser = Parser::CurrentRuby.new
+
+    rewriter.rewrite(buffer, parser.parse(buffer))
   end
 end
--- spec/nice_literal_spec.rb
+++ spec/nice_literal_spec.rb
@@ -2,8 +2,8 @@ require_relative "spec_helper"
 
 describe ZombieKiller do
   it "translates Ops.add of two string literals" do
-    c1 = "Ops.add('Hello', 'World')"
-    c2 = "'Hello' + 'World'"
+    c1 = 'Ops.add("Hello", "World")'
+    c2 = '"Hello" + "World"'
     expect(ZombieKiller.new.kill(c1)).to eq c2
   end
 end
