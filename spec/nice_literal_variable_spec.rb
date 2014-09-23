c7177003a80bb7515c4522c97a291f43c9b5b7b2
Martin Vidner <mvidner@suse.cz>
2014-09-23 10:58:13 +0200

    more passing tests for variables

--- spec/nice_literal_variable_spec.rb
+++ spec/nice_literal_variable_spec.rb
@@ -1,15 +1,32 @@
 require_relative "spec_helper"
 
 describe ZombieKiller do
-  it "translates Ops.add(variable set to literal, string literal)" do
-    c1 = 'v = "Hello"; Ops.add(v, "World")'
-    c2 = 'v = "Hello"; v + "World"'
-    expect(ZombieKiller.new.kill(c1)).to eq c2
+  context "one argument is a variable set to a literal" do
+    it "translates Ops.add(variable, literal)" do
+      c1 = 'v = "Hello"; Ops.add(v, "World")'
+      c2 = 'v = "Hello"; v + "World"'
+      expect(ZombieKiller.new.kill(c1)).to eq c2
+    end
+
+    it "translates Ops.add(literal, variable)" do
+      c1 = 'v = "World"; Ops.add("Hello", v)'
+      c2 = 'v = "World"; "Hello" + v'
+      expect(ZombieKiller.new.kill(c1)).to eq c2
+    end
   end
 
-  it "translates Ops.add(string literal, variable set to literal)" do
-    c1 = 'v = "World"; Ops.add("Hello", v)'
-    c2 = 'v = "World"; "Hello" + v'
-    expect(ZombieKiller.new.kill(c1)).to eq c2
+  context "argument is variable set to a literal, passed via another var" do
+    it "translates Ops.add(variable, literal)" do
+      c1 = 'v = "Hello"; v2 = v; Ops.add(v2, "World")'
+      c2 = 'v = "Hello"; v2 = v; v2 + "World"'
+      expect(ZombieKiller.new.kill(c1)).to eq c2
+    end
+  end
+
+  context "one argument is a variable set to a literal but mutated" do
+    it "does not translate Ops.add(variable, literal)" do
+      c = 'v = "Hello"; v = f(v); Ops.add(v, "World")'
+      expect(ZombieKiller.new.kill(c)).to eq c
+    end
   end
 end
