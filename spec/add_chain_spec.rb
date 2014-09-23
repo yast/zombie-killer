b0f7277fabc5b474d95153e420e4f0b57b64fa76
Martin Vidner <mvidner@suse.cz>
2014-09-23 13:51:38 +0200

    use correct quotes; skip what will not work yet

--- spec/add_chain_spec.rb
+++ spec/add_chain_spec.rb
@@ -2,15 +2,18 @@ require_relative "spec_helper"
 
 describe ZombieKiller do
   subject { ZombieKiller.new }
-  it "translates a chain of Ops.add of literals" do
-    c1 = "Ops.add(Ops.add('Hello', ' '), 'World')"
-    c2 = "('Hello' + ' ') + 'World'"
-    expect(subject.kill(c1)).to eq c2
-  end
 
-  it "translates a right-assoc chain of Ops.add of literals" do
-    c1 = "Ops.add('Hello', Ops.add(' ', 'World'))"
-    c2 = "'Hello' + (' ' + 'World')"
-    expect(subject.kill(c1)).to eq c2
+  context "while we keep operating on the original AST, recursion won't work" do
+    xit 'translates a chain of Ops.add of literals' do
+      c1 = 'Ops.add(Ops.add("Hello", " "), "World")'
+      c2 = '("Hello" + " ") + "World"'
+      expect(subject.kill(c1)).to eq c2
+    end
+
+    xit "translates a right-assoc chain of Ops.add of literals" do
+      c1 = 'Ops.add("Hello", Ops.add(" ", "World"))'
+      c2 = '"Hello" + (" " + "World")'
+      expect(subject.kill(c1)).to eq c2
+    end
   end
 end
