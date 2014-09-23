eda1756dcf4380a9d6bdd6f7806b8014e9a3dc1e
Martin Vidner <mvidner@suse.cz>
2014-09-23 16:31:37 +0200

    remove old examples already captured in specs

--- examples/add.rb
+++ examples/add.rb
@@ -1,22 +1,5 @@
-def literal_is_not_nil
-  s = "Hello"
-  # s must not "go bad"
-  s = Ops.add(s, "World")
-end
-
-def translated_literal_is_not_nil
-  s = _("Hello")
-  s = Ops.add(s, _("World"))
-end
-
 def condition_checks_value_is_not_nil(s)
   if s != nil
     s2 = Ops.add(s, "foo")
   end
 end
-
-# CANNOT do
-def foo(a)
-  b = Ops.add(a, 1)
-end
-
