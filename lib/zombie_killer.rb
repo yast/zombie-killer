8fa6a2e2bc2f0cb1dc856b3006d221f110aa007c
Martin Vidner <mvidner@suse.cz>
2014-09-23 10:51:59 +0200

    nice_variable; previous test PASSES

--- lib/zombie_killer.rb
+++ lib/zombie_killer.rb
@@ -2,13 +2,48 @@ require "parser"
 require "parser/current"
 require "unparser"
 
+# Tracks niceness for local variables visible at certain point
+class VariableScope < Hash
+  # @return [Boolean] nice
+  def [](varname)
+    super
+  end
+
+  # Set niceness for a variable
+  def []=(varname, nice)
+    super
+  end
+end
+
 class ZombieKillerRewriter < Parser::Rewriter
+  def initialize
+    outer_scope = VariableScope.new
+    @scopes = [outer_scope]
+  end
+
+  # currently visible scope
+  def scope
+    @scopes.last
+  end
+
+  def on_def(node)
+    @scopes.push VariableScope.new
+    super
+    @scopes.pop
+  end
+
+  # local(?) variable assignment
+  def on_vasgn(node)
+    name, value = * node
+    scope[name] = nice(value)
+  end
+
   def on_send(node)
     if is_call(node, :Ops, :add)
       new_op = :+
 
       ops, add, a, b = *node
-      if nice_literal(a) && nice_literal(b)
+      if nice(a) && nice(b)
         replace_node node, Parser::AST::Node.new(:send, [a, new_op, b])
       end
     end
@@ -24,8 +59,16 @@ class ZombieKillerRewriter < Parser::Rewriter
       n_message == message
   end
 
+  def nice(node)
+    nice_literal(node) || nice_variable(node)
+  end
+
   def nice_literal(node)
     node.type == :str
+  end
+
+  def nice_variable(node)
+    node.type == :lvar && scope[node.children.first]
   end
 
   def replace_node(old_node, new_node)
