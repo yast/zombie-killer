0e5b380b6f41038f8ebe844cd6ea87bfcb9b3486
Martin Vidner <mvidner@suse.cz>
2014-09-25 16:18:52 +0200

    zk --stats: Also print statistics about node types.
    implemented by NodeTypeCounter

--- lib/zombie_killer.rb
+++ lib/zombie_killer.rb
@@ -3,6 +3,7 @@ require "parser/current"
 require "set"
 require "unparser"
 
+require_relative "code_histogram"
 require_relative "version"
 
 # Tracks niceness for local variables visible at certain point
@@ -15,6 +16,32 @@ class VariableScope < Hash
   # Set niceness for a variable
   def []=(varname, nice)
     super
+  end
+end
+
+class NodeTypeCounter < Parser::Rewriter
+  attr_reader :node_types
+
+  def initialize(filename)
+    @node_types = CodeHistogram.new
+    @filename = filename
+  end
+
+  def process(node)
+    return if node.nil?
+    @node_types.increment(node.type)
+    super
+  end
+
+  def print(io)
+    parser = Parser::CurrentRuby.new
+    buffer = Parser::Source::Buffer.new(@filename)
+    buffer.read
+    ast    = parser.parse(buffer)
+
+    process(ast)
+
+    @node_types.print_by_frequency(io)
   end
 end
 
