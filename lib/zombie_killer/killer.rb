f7663ec2c7839ff88483dbb6f94709867790bdbc
Martin Vidner <martin@vidner.net>
2018-11-01 15:50:39 +0100

    Added zk -e, EagerRewriter (don't care about niceness, replace all)

--- lib/zombie_killer/killer.rb
+++ lib/zombie_killer/killer.rb
@@ -1,14 +1,25 @@
 require "parser"
 require "parser/current"
 
+require_relative "eager_rewriter"
 require_relative "rewriter"
 
+# The main class called from the CLI
 class ZombieKiller
+  # @return [Boolean] use the EagerRewriter
+  attr_reader :eager
+
+  def initialize(eager: false)
+    @eager = eager
+  end
+
+  # @param code [String]
+  # @param filename [String]
   # @returns new string
   def kill_string(code, filename = "(inline code)", unsafe: false)
     fixed_point(code) do |c|
       parser   = Parser::CurrentRuby.new
-      rewriter = ZombieKillerRewriter.new(unsafe: unsafe)
+      rewriter = eager ? EagerRewriter.new : ZombieKillerRewriter.new(unsafe: unsafe)
       buffer   = Parser::Source::Buffer.new(filename)
       buffer.source = c
       rewriter.rewrite(buffer, parser.parse(buffer))
